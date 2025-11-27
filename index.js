529b69c37d5c7a29d8621994ffd5e18d5140b990
SachinGusain <gusainfitness@gmail.com>
2025-11-27 22:04:22 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello DevOps!");
+console.log("Iam learning CI/CD pipeline using Github Action");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting the test");
+setTimeout(() => console.log('Waiting 3 second...'),3000);
+console.log("Test complete!");
